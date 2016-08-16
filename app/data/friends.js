6d061d09479d5565a28292c334d5d04935f5062f
David Li <jdavidli01@outlook.com>
2016-08-16 16:20:49 -0400

    survey, and home

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+[
+{
+name: "Ahmed",
+photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+scores: [
+"5",
+"1",
+"4",
+"4",
+"5",
+"1",
+"2",
+"5",
+"4",
+"1"
+]
+},
+{
+name: "jimmy",
+photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+scores: [
+"5",
+"2",
+"2",
+"2",
+"4",
+"1",
+"3",
+"2",
+"5",
+"5"
+]
+},
+{
+name: "david",
+photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+scores: [
+"4",
+"4",
+"2",
+"3",
+"2",
+"2",
+"3",
+"2",
+"4",
+"5"
+]
+},
+{
+name: "joanne",
+photo: "http://matt-bajorek-portfolio.herokuapp.com/img/profilepic1.jpg",
+scores: [
+"5",
+"3",
+"4",
+"2",
+"3",
+"4",
+"1",
+"4",
+"2",
+"4"
+]
+}
+]
